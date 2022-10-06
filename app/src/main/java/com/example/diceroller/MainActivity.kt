a88776d0fcf8124303e699223af16f3fb43869ca
Sergio Velez <sergiovelez749@gmail.com>
2022-10-05 19:40:53 -0500

    Trabajo Autonomo - Aplicacion Movil

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * Esta actividad permite al usuario tirar un dado y ver el resultado
+ * en la pantalla.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+        // Haz una tirada de dados cuando se inicia la aplicación
+        rollDice()
+    }
+
+    /**
+     * Tira los dados y actualiza la pantalla con el resultado.
+     */
+    private fun rollDice() {
+        // Crea un nuevo objeto Dice con 6 lados y hazlo rodar
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Actualizar la pantalla con la tirada de dados
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // Determine qué ID de recurso dibujable usar en función de la tirada de dados
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Actualizar ImageView con el ID de recurso dibujable correcto
+        diceImage.setImageResource(drawableResource)
+
+        // Actualizar la descripción del contenido
+        diceImage.contentDescription = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
